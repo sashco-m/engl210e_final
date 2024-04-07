752d083f51a3e5c2d39b51bff1fef27fff0e928a
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2024-04-07 00:54:40 +0000

    change name again

--- engl210e_final/auth.py
+++ engl210e_final/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from ENGL210E_FINAL.db import get_token_db
+from engl210e_final.db import get_token_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
